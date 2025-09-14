56751a8f8f38f63d957108750ddf0590646f70cb
Pro Jokhoe <Pro.jokhoe2@gmail.com>
2025-09-14 21:22:33 +0200

    Simplify Jest configuration by removing settings
    Signed-off-by: Pro Jokhoe <Pro.jokhoe2@gmail.com>

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,8 @@
+// jest.config.mjs
 export default {
   testEnvironment: "node",
   reporters: [
     "default",
     ["jest-junit", { outputDirectory: "reports/junit", outputName: "junit.xml" }],
   ],
-  roots: ["<rootDir>/tests", "<rootDir>/__tests__"],
-  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
-  passWithNoTests: true,
 };
