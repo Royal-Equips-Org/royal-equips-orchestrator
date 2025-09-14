6d73cd2539f9da39e98dab8e7e722d5bdf934816
Pro Jokhoe <Pro.jokhoe2@gmail.com>
2025-09-14 20:22:52 +0200

    Update jest.config.js for test configuration
    Signed-off-by: Pro Jokhoe <Pro.jokhoe2@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ export default {
   testEnvironment: "node",
   reporters: [
     "default",
-    ["jest-junit", { outputDirectory: "reports/junit", outputName: "junit.xml" }]
+    ["jest-junit", { outputDirectory: "reports/junit", outputName: "junit.xml" }],
   ],
-  passWithNoTests: true
+  roots: ["<rootDir>/tests", "<rootDir>/__tests__"],
+  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+  passWithNoTests: true,
 };
